a7c1441816a3375f43c5cabfaf398f646cfee358
CorpusMartinez <corpus.martinez@me.com>
2016-02-22 23:25:59 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,17 +4,6 @@ session_start();
 include("sesion.php");
 include("header.php");
 
-switch($t) {
-	case              '': include('resumen.php');        break;
-	case   'solicitudes': include('solicitudes.php');    break;
-	case       'visitas': include('recorridos.php');     break;
-	case     'productos': include('productos.php');      break;
-	case       'equipos': include('equipos.php');        break;
-	case  'propietarios': include('propietarios.php');   break;
-	case     'contactos': include('contactos.php');      break;
-	case      'usuarios': include('usuarios.php');       break;
-	case     'actividad': include('actividad.php');      break;
-	case 'configuracion': include('configuracion.php');	 break;
-}
+echo $_SESSION['username'];
 
 include('footer.php');
